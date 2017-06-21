326979e27ddc27a1c74919ddf43e6e8b720a77b5
Shirley Xiao <shirley.xiao@qochealth.com>
2017-06-21 13:07:44 -0400

    modified MedicationList

--- src/Components/MedicationList/MedicationList.jsx
+++ src/Components/MedicationList/MedicationList.jsx
@@ -1,53 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MedicationRow from '../MedicationRow/MedicationRow.jsx';
 
+const MedicationList = ( props ) => {
+  const {
+    onDelEvent,
+    onSelection,
+    puffOnChange,
+    timesOnChange,
+    doseICSOnChange,
+    onSubmit,
+  } = props;
 
-class MedicationList extends Component {
-	render() {
-		const onDelEvent = this.props.onDelRow;
-		const onSelection = this.props.onSelection;
-		const puffOnChange = this.props.puffOnChange;
-    const timesOnChange = this.props.timesOnChange;
-    const doseICSOnChange = this.props.doseICSOnChange;
-		const onSubmit = this.props.onSubmit;
-
-		return (
-			<div className="app__body" onSubmit={onSubmit}>
-        <ul className="header">
-          <li>Puffs Per Time</li>
-          <li>Times Per Day</li>
-          <li>Dose ICS</li>
-        </ul>
-				<MedicationRow
-          onDelEvent={onDelEvent}
-          onSelection={onSelection}
-          onChangePuffs={puffOnChange}
-          onChangeTimes={timesOnChange}
-          onChangeDose={doseICSOnChange} />
-			</div>
-		);
-	}
-}
+ return (
+   <div className="app__body" onSubmit={onSubmit}>
+     <ul className="header">
+       <li>Puffs Per Time</li>
+       <li>Times Per Day</li>
+       <li>Dose ICS</li>
+     </ul>
+     <MedicationRow
+       onDelEvent={onDelEvent}
+       onSelection={onSelection}
+       onChangePuffs={puffOnChange}
+       onChangeTimes={timesOnChange}
+       onChangeDose={doseICSOnChange}/>
+   </div>
+ );
+};
 
 export default MedicationList;
-
-/*
- const MedicationList = (onDelEvent, onSelection, puffOnChange, timesOnChange, doseICSOnChange, onSubmit) =>
- {
- return (
- <div className="app__body" onSubmit={onSubmit}>
- <ul className="header">
- <li>Puffs Per Time</li>
- <li>Times Per Day</li>
- <li>Dose ICS</li>
- </ul>
- <AddRow
- onDelEvent={onDelEvent}
- onSelection={onSelection}
- onChangePuffs={puffOnChange}
- onChangeTimes={timesOnChange}
- onChangeDose={doseICSOnChange}/>
- </div>
- );
- };
- */
