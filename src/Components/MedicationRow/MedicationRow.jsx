54875926f995f983ffb96cce3d2b383126b4aa75
Shirley Xiao <shirley.xiao@qochealth.com>
2017-06-21 13:49:00 -0400

    reformated spacing for return

--- src/Components/MedicationRow/MedicationRow.jsx
+++ src/Components/MedicationRow/MedicationRow.jsx
@@ -15,38 +15,38 @@ const MedicationRow = ( props ) => {
     patientMedications,
   } = props;
 
-    return (
-			<div className="row">
-        <InputField
-          fieldName="row__textfield--puff"
-          value={puffValue}
-          onChange={puffOnChange}
-        />
+  return (
+    <div className="row">
+      <InputField
+        fieldName="row__textfield--puff"
+        value={puffValue}
+        onChange={puffOnChange}
+      />
 
-        <InputField
-          fieldName="row__textfield--times"
-          value={timesPerDayValue}
-          onChange={timesOnChange}
-        />
+      <InputField
+        fieldName="row__textfield--times"
+        value={timesPerDayValue}
+        onChange={timesOnChange}
+      />
 
-        <InputField
-          fieldName="row__body__textfield--doseICS"
-          value={doseICSValue}
-          onChange={doseICSOnChange}
-        />
+      <InputField
+        fieldName="row__body__textfield--doseICS"
+        value={doseICSValue}
+        onChange={doseICSOnChange}
+      />
 
-        <select className="row__select" value={patientMedications} onChange={onSelection}>
-          <option>-Select Medication-</option>
-          <option>ddd</option>
-          <option>b</option>
-          <option>c</option>
-        </select>
+      <select className="row__select" value={patientMedications} onChange={onSelection}>
+        <option>-Select Medication-</option>
+        <option>ddd</option>
+        <option>b</option>
+        <option>c</option>
+      </select>
 
-        <div className="row__button">
-          <button onClick={onDelEvent}>Delete Row</button>
-        </div>
+      <div className="row__button">
+        <button onClick={onDelEvent}>Delete Row</button>
       </div>
-		);
+      </div>
+   );
 };
 
 export default MedicationRow;
