e36923057ac17147847e9338d0d79a3402a4f98f
Shirley Xiao <shirley.xiao@qochealth.com>
2017-06-22 11:59:03 -0400

    added component Stack

---
+++ src/Components/Stack/Stack.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+const Row = ( {elements} ) => {
+  console.log(elements);
+
+  const items = _
+    .chain( elements )
+      .map( ( element, index ) => (
+       <li key={index}>{element}</li>
+      ))
+    .value();
+
+ return (
+   <div className="row">
+      <ul>
+        {items}
+      </ul>
+   </div>
+  );
+};
+
+Row.propTypes = {
+  elements: PropTypes.array,
+};
+
+Row.defaultProps = {
+  elements: null,
+};
+
+export default Row;
