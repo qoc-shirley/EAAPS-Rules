4887b3513b98409e85068fd113a97a960751a230
Shirley Xiao <shirley.xiao@qochealth.com>
2017-06-27 12:38:36 -0400

    edited header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return(
     <div className="header">
       <div className="header__logo">
-        <img src={logo} className="header__logo" alt="logo"/>
+        <img src={logo} className="header__logo" alt="logo" />
       </div>
       <div className="header__heading">
         <h2>EAAPs Escalation Rules</h2>
