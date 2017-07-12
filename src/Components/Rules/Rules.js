d6a0eaf3da90b2cd52eab02d94bd7ae6f0e4a4c0
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-12 11:20:11 -0400

    edited rule 12

--- src/Components/Rules/Rules.js
+++ src/Components/Rules/Rules.js
@@ -704,12 +704,23 @@ export const rule12 = (patientMedications, masterMedications) => {
                 if (!_.isEmpty(_.filter(filteredMedication, (medication) => {
                     return medication.device === isLaba.device || medication.device === isICS.device
                   }))) {
-                  result.push(_.max(_.filter(filteredMedication, {device: patientMedication.device}), 'doseICS'));
+                  //test to see which device can be put into the lowest possible dose within the medium dose category
+                  result.push();
                 }
                 else {
+                  //increase the original medication ICD to lowest possible dose within the medium dose category + recommend LTRA
+                  //match ICS device can be put into the lowest possible dose within the medium dose category
+                  //match the ICS medication with ^ device
+                  //match timesPerDay
+                  //minimize required ICS puffPerTime
                 }
               }
               else {
+                //increase original medication ICS to lowest dose in medium category + recommend LABA
+                //match ICS Org device
+                //attempt to match ICS ORG dosePerPuff
+                //match timesPerDay
+                //minimize required ICS puffPerTime
               }
             }
             else {
