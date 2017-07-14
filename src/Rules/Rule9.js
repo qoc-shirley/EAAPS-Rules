a7cbfe26908ae17023b4af0a7d445c1e7886c4d3
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-14 16:18:24 -0400

    tested and edite rule 9

--- src/Rules/Rule9.js
+++ src/Rules/Rule9.js
@@ -5,31 +5,23 @@ import * as adjust from './Library/AdjustICSDose';
 const rule9 = (patientMedications) => {
   return _.chain(patientMedications)
     .reduce((result, patientMedication) => {
-    console.log("asdfasf");
-      if (patientMedication.name === "symbicort" && patientMedication.controller === "controller,reliever" &&
+      if (patientMedication.name === "symbicort" && patientMedication.function === "controller,reliever" &&
         ( calculate.ICSDose(patientMedication) < patientMedication.maxGreenICS ) &&
         _.some(patientMedications, {chemicalType: "ltra"})) {
         console.log("hello");
         if (_.isEmpty(adjust.ICSDose(patientMedication, "highest"))) {
           console.log("a");
-          result.push(
-            _.max(
-              _.filter(patientMedications, (medication) => {
-                return medication.name === "symbicort" &&
-                  medication.controller === "controller,reliever" &&
-                  (calculate.ICSDose(medication) < medication.maxGreenICS)
-              }),
-              'doseICS'));
-          result.push(patientMedications);
+          result.push(patientMedication);
           result.push(_.filter(patientMedications, {chemicalType: "ltra"}));
         }
         else {
-          console.log("a");
-          result.push(patientMedications);
+          console.log("b");
           result.push(adjust.ICSDose(patientMedication, "highest"));
           result.push(_.filter(patientMedications, {chemicalType: "ltra"}));
         }
       }
+      result = _.flatten(result);
+      result = _.uniqBy(result, "id");
       return result;
     }, [])
     .value();
