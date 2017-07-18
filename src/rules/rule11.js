901c065f281a0d96bbc9602069f7dcad2b25b907
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-18 16:58:09 -0400

    edited rule11

--- src/rules/rule11.js
+++ src/rules/rule11.js
@@ -1,33 +1,36 @@
 import _ from 'lodash';
 import masterMedications from '../medicationData/medicationData';
 
-const getLabaICSAndICS = (patientMedications) => {
-  let result = [];
-  return _.chain(patientMedications)
+const getLabaICSAndICS = ( patientMedications ) => {
+  const result = [];
+
+  return _.chain( patientMedications )
     .filter(
-      _.partial((medicationElements, patientMedication) => {
-        if (patientMedication.chemicalType === "ICS") {
-          result.push(patientMedication);
+      _.partial( ( medicationElements, patientMedication ) => {
+        if ( patientMedication.chemicalType === 'ICS' ) {
+          result.push( patientMedication );
         }
-        else if (patientMedication.chemicalType === "laba,ICS") {
-          result.push(patientMedication);
+        else if ( patientMedication.chemicalType === 'laba,ICS' ) {
+          result.push( patientMedication );
         }
-      }, masterMedications))
-    .concat(result)
+      }, masterMedications ) )
+    .concat( result )
     .flatten()
     .value();
 };
 
-const rule11 = (patientMedications, masterMedications) => {
+const rule11 = ( patientMedications, masterMedications ) => {
   let newMedication = [];
-  let filteredPatientMedications = getLabaICSAndICS(patientMedications);
-  if (_.find(filteredPatientMedications, {chemicalType: "ICS"}) && _.find(filteredPatientMedications, {chemicalType: "laba,ICS"})) {
-    newMedication = _.filter(masterMedications, {name: "singulair"});
+  let filteredPatientMedications = getLabaICSAndICS( patientMedications );
+  if ( _.find( filteredPatientMedications, { chemicalType: 'ICS' } ) &&
+    _.find( filteredPatientMedications, { chemicalType: 'laba,ICS' } ) ) {
+    newMedication = _.filter( masterMedications, { name: 'singulair' } );
   }
   else {
     filteredPatientMedications = [];
   }
-  return _.concat(newMedication, filteredPatientMedications)
+
+  return _.concat( newMedication, filteredPatientMedications );
 };
 
 export default rule11;
