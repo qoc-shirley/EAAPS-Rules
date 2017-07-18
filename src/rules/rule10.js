f9e22bccfcf6c30e0eaa94cdc39d902a7d472bdb
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-18 16:59:52 -0400

    edited rule 10

--- src/rules/rule10.js
+++ src/rules/rule10.js
@@ -1,26 +1,28 @@
 import _ from 'lodash';
 import * as calculate from './library/calculateICSDose';
 
-const rule10 = (patientMedications, masterMedications) => {
+const rule10 = ( patientMedications, masterMedications ) => {
   const consultRespirologist = _
-    .chain(patientMedications)
+    .chain( patientMedications )
     .filter(
-      _.partial((medicationElements, patientMedication) => {
-        if (patientMedication.name === "symbicort" &&
-          patientMedication.function === "controller,reliever" &&
-          ( calculate.patientICSDose(patientMedication) >= patientMedication.maxGreenICS )) {
-          if (_.find(patientMedications, {chemicalType: "ltra"})) {
+      _.partial( ( medicationElements, patientMedication ) => {
+        if ( patientMedication.name === 'symbicort' &&
+          patientMedication.function === 'controller,reliever' &&
+          ( calculate.patientICSDose( patientMedication ) >= patientMedication.maxGreenICS ) ) {
+          if ( _.find( patientMedications, { chemicalType: 'ltra' } ) ) {
             return true;
           }
+
           return false;
         }
-      }, masterMedications)
+      }, masterMedications ),
     )
     .value();
 
-  if (!_.isEmpty(consultRespirologist)) {
-    return consultRespirologist.concat("consult a respirologist");
+  if ( !_.isEmpty( consultRespirologist ) ) {
+    return consultRespirologist.concat( 'consult a respirologist' );
   }
+
   return [];
 };
 
