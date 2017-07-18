f6f8c85d4304aebd974027dac9e3fd34af4f9586
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-18 15:55:22 -0400

    edited rulesApp

--- src/Containers/RulesApp/RulesApp.jsx
+++ src/Containers/RulesApp/RulesApp.jsx
@@ -2,26 +2,27 @@ import { connect } from 'react-redux';
 import App from '../../Components/App/App';
 import * as actions from '../../redux/App/actions';
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => ( {
   medication: state.medication,
-});
+} );
 
 const mapDispatchToProps = dispatch => ( {
-  appendMedicationList: (medicationRow) => dispatch( actions.appendMedicationList(medicationRow) ),
-  onChangePuffValue: (index, value) => dispatch( actions.onChangePuffValue(index,value) ),
-  onChangeTimesPerDayValue: (index, value) => dispatch( actions.onChangeTimesPerDayValue(index, value) ),
-  onChangeDoseICS: (index, value) => dispatch( actions.onChangeDoseICS(index, value) ),
-  onChangeChemicalICS: (index, value) => dispatch( actions.onChangeChemicalICS(index, value) ),
-  onChangeChemicalLABA: (index, value) => dispatch( actions.onChangeChemicalLABA(index, value) ),
-  onChangeDeviceName: (index, value) => dispatch( actions.onChangeDeviceName(index, value) ),
-  onChangeMedicationName: (index, value) => dispatch( actions.onChangeMedicationName(index, value) ),
-  onDeleteRow: (index) => dispatch( actions.onDeleteRow(index) ),
-  displayResult: (event) => dispatch( actions.displayResult(event) ),
-  getPatientMedications: (medications) => dispatch( actions.getPatientMedications(medications) ),
-  saveRecommendation: (rule, medications) => dispatch( actions.saveRecommendation(rule, medications) ),
+  appendMedicationList: ( medicationRow ) => dispatch( actions.appendMedicationList( medicationRow ) ),
+  onChangePuffValue: ( index, value ) => dispatch( actions.onChangePuffValue( index, value ) ),
+  onChangeTimesPerDayValue: ( index, value ) => dispatch( actions.onChangeTimesPerDayValue( index, value ) ),
+  onChangeDoseICS: ( index, value ) => dispatch( actions.onChangeDoseICS( index, value ) ),
+  onChangeChemicalICS: ( index, value ) => dispatch( actions.onChangeChemicalICS( index, value ) ),
+  onChangeChemicalLABA: ( index, value ) => dispatch( actions.onChangeChemicalLABA( index, value ) ),
+  onChangeDeviceName: ( index, value ) => dispatch( actions.onChangeDeviceName( index, value ) ),
+  onChangeMedicationName: ( index, value ) => dispatch( actions.onChangeMedicationName( index, value ) ),
+  onDeleteRow: ( index ) => dispatch( actions.onDeleteRow( index ) ),
+  displayResult: ( event ) => dispatch( actions.displayResult( event ) ),
+  getPatientMedications: ( medications ) => dispatch( actions.getPatientMedications( medications ) ),
+  saveRecommendation: ( rule, medications ) => dispatch( actions.saveRecommendation( rule, medications ) ),
   onClickClear: () => dispatch( actions.onClickClear() ),
 } );
 
-const RulesApp = connect(mapStateToProps, mapDispatchToProps)(App);
+const RulesApp = connect( mapStateToProps, mapDispatchToProps )( App );
 
 export default RulesApp;
+
