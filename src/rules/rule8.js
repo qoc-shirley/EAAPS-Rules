2a57eb89476c2b9337b298b4c0f7a021935cd985
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-19 10:00:23 -0400

    edited rule8

--- src/rules/rule8.js
+++ src/rules/rule8.js
@@ -1,26 +1,29 @@
 import _ from 'lodash';
 import * as categorize from './library/categorizeDose';
 
-const rule8 = (patientMedications, masterMedications) => {
-  const isSMARTMediumOrHigh = _.chain(patientMedications)
-    .filter((patientMedication) => {
-      if (patientMedication.name === "symbicort" &&
-        patientMedication.function === "controller,reliever" &&
-        (categorize.patientICSDose(patientMedication) === "medium" || categorize.patientICSDose(patientMedication) === "high")) {
+const rule8 = ( patientMedications, masterMedications ) => {
+  const isSMARTMediumOrHigh = _.chain( patientMedications )
+    .filter( ( patientMedication ) => {
+      if ( patientMedication.name === 'symbicort' &&
+        patientMedication.function === 'controller,reliever' &&
+        ( categorize.patientICSDose( patientMedication ) === 'medium' ||
+        categorize.patientICSDose( patientMedication ) === 'high' ) ) {
         return true;
       }
+
       return false;
-    })
+    } )
     .value();
 
-  if (!_.isEmpty(isSMARTMediumOrHigh)) {
+  if ( !_.isEmpty( isSMARTMediumOrHigh ) ) {
     return isSMARTMediumOrHigh
       .concat(
-        _.chain(masterMedications)
-          .filter({name: "singulair"})
-          .value()
-      )
+        _.chain( masterMedications )
+          .filter( { name: 'singulair' } )
+          .value(),
+      );
   }
+
   return [];
 };
 
