856f7dbb20cadd8303c00ca5fcb7f9d5e8585b73
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-19 10:04:45 -0400

    edited rule6

--- src/rules/rule6.js
+++ src/rules/rule6.js
@@ -1,36 +1,39 @@
 import _ from 'lodash';
 import * as calculate from './library/calculateICSDose';
 
-const rule6 = (patientMedications) => {
+const rule6 = ( patientMedications ) => {
 
   const consultRespirologist = _
-    .chain(patientMedications)
-    .filter((patientMedication) => {
-      const filterChemicalTypeLtra = _.filter(patientMedications, {chemicalType: "ltra"});
+    .chain( patientMedications )
+    .filter( ( patientMedication ) => {
+      const filterChemicalTypeLtra = _.filter( patientMedications, { chemicalType: 'ltra' } );
       const isFilteredLtraGreatermaxGreenICS = _
-        .chain(filterChemicalTypeLtra)
-        .filter((patientMedication) => {
-          if (calculate.patientICSDose(patientMedication) >= patientMedication.maxGreenICS) {
+        .chain( filterChemicalTypeLtra )
+        .filter( ( patientMedication ) => {
+          if ( calculate.patientICSDose( patientMedication ) >= patientMedication.maxGreenICS ) {
             return true;
           }
+
           return false;
-        })
+        } )
         .isEmpty()
         .value();
-      if (patientMedication.name !== "symbicort" &&
-        (patientMedication.chemicalType === "laba,ICS" ||
-        patientMedication.chemicalType === "ICS" ||
-        patientMedication.chemicalType === "laba" ) &&
-        filterChemicalTypeLtra && !isFilteredLtraGreatermaxGreenICS) {
+      if ( patientMedication.name !== 'symbicort' &&
+        ( patientMedication.chemicalType === 'laba,ICS' ||
+        patientMedication.chemicalType === 'ICS' ||
+        patientMedication.chemicalType === 'laba' ) &&
+        filterChemicalTypeLtra && !isFilteredLtraGreatermaxGreenICS ) {
         return true;
       }
-      return false
-    })
+
+      return false;
+    } )
     .value();
 
-  if (!_.isEmpty(consultRespirologist)) {
-    return consultRespirologist.concat("consult a respirologist");
+  if ( !_.isEmpty( consultRespirologist ) ) {
+    return consultRespirologist.concat( 'consult a respirologist' );
   }
+
   return [];
 };
 
