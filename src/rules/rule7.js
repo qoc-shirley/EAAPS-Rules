2e00dad323130a56cc608697f6f0da80f2d767c5
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-19 10:02:40 -0400

    edited rule7

--- src/rules/rule7.js
+++ src/rules/rule7.js
@@ -2,28 +2,29 @@ import _ from 'lodash';
 import * as categorize from './library/categorizeDose';
 import * as adjust from './library/adjustICSDose';
 
-const rule7 = (patientMedications) => {
-  return _.chain(patientMedications)
-    .reduce((result, patientMedication) => {
-      if (patientMedication.name === "symbicort" &&
-        patientMedication.function === "controller,reliever" &&
-        categorize.patientICSDose(patientMedication) === "low") {
-        if (adjust.ICSDose(patientMedication, "lowestMedium") === []) {
+const rule7 = ( patientMedications ) => {
+  return _.chain( patientMedications )
+    .reduce( ( result, patientMedication ) => {
+      if ( patientMedication.name === 'symbicort' &&
+        patientMedication.function === 'controller,reliever' &&
+        categorize.patientICSDose( patientMedication ) === 'low' ) {
+        if ( adjust.ICSDose( patientMedication, 'lowestMedium' ) === [] ) {
           result.push(
             _.max(
-              _.filter(patientMedications, (medication) => {
-                return medication.name === "symbicort" &&
-                  medication.function === "controller,reliever" &&
-                  categorize.patientICSDose(medication) === "low"
-              }),
-              'doseICS'));
+              _.filter( patientMedications, ( medication ) => {
+                return medication.name === 'symbicort' &&
+                  medication.function === 'controller,reliever' &&
+                  categorize.patientICSDose( medication ) === 'low';
+              } ),
+              'doseICS' ) );
         }
         else {
-          result.push(adjust.ICSDose(patientMedication, "lowestMedium"));
+          result.push( adjust.ICSDose( patientMedication, 'lowestMedium' ) );
         }
       }
+
       return result;
-    }, [])
+    }, [] )
     .value();
 };
 
