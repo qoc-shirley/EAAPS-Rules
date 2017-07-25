345b27b61ab1c562895b4f7c4108f1bb29d6e50d
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-25 10:08:20 -0400

    edited Rule 6 and tested also added margin top to buttons in medication table

--- src/rules/rule6.js
+++ src/rules/rule6.js
@@ -3,38 +3,49 @@ import * as calculate from './library/calculateICSDose';
 
 const rule6 = ( patientMedications ) => {
 
-  const consultRespirologist = _
+  // const consultRespirologist = _
+  return _
     .chain( patientMedications )
     .filter( ( patientMedication ) => {
       const filterChemicalTypeLtra = _.filter( patientMedications, { chemicalType: 'ltra' } );
-      const isFilteredLtraGreatermaxGreenICS = _
-        .chain( filterChemicalTypeLtra )
-        .filter( ( patientMedication ) => {
-          if ( calculate.patientICSDose( patientMedication ) >= patientMedication.maxGreenICS ) {
-            return true;
-          }
+      const filterChemicalTypeLaba = _.filter( patientMedications, { chemicalType: 'laba' } );
 
-          return false;
-        } )
-        .isEmpty()
-        .value();
+      // const isFilteredLtraGreatermaxGreenICS = _
+      //   .chain( filterChemicalTypeLtra )
+      //   .filter( ( patientMedication ) => {
+      //     if ( calculate.patientICSDose( patientMedication ) >= patientMedication.maxGreenICS ) {
+      //       return true;
+      //     }
+			//
+      //     return false;
+      //   } )
+      //   .isEmpty()
+      //   .value();
       if ( patientMedication.name !== 'symbicort' &&
         ( patientMedication.chemicalType === 'laba,ICS' ||
-          ( patientMedication.chemicalType === 'ICS' && patientMedication.chemicalType === 'laba' )
+        ( !_.isEmpty( filterChemicalTypeLaba ) && patientMedication.chemicalType === 'ICS' )
         ) &&
-        filterChemicalTypeLtra && !isFilteredLtraGreatermaxGreenICS ) {
+        calculate.patientICSDose( patientMedication ) >= patientMedication.maxGreenICS &&
+        !_.isEmpty( filterChemicalTypeLtra ) ) {
         return true;
       }
 
       return false;
     } )
+    .thru( ( arr ) => {
+      if ( _.size( arr ) ) {
+        return [['Consult a respirologist']];
+      }
+
+      return [];
+    } )
     .value();
 
-  if ( !_.isEmpty( consultRespirologist ) ) {
-    return consultRespirologist.concat( 'consult a respirologist' );
-  }
-
-  return [];
+  // if ( !_.isEmpty( consultRespirologist ) ) {
+  //   return consultRespirologist.concat( 'consult a respirologist' );
+  // }
+	//
+  // return [];
 };
 
 export default rule6;
