82d5e50f3f231e8fd772dcf5bf10205d1dab2cc3
Shirley Xiao <shirley.xiao@qochealth.com>
2017-07-26 16:01:02 -0400

    copied and pasted rule -1 from escalation rules

---
+++ src/rules/de-escalation/ruleMinus1.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+const ruleMinus1 = ( patientMedications ) => {
+  return _.chain( patientMedications )
+    .filter( ( patientMedication ) => {
+      return patientMedication.chemicalType === 'laac';
+    } )
+    .value();
+};
+
+export default ruleMinus1;
