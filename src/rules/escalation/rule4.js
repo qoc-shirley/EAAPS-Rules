e0e0a01adda51754ac5773283c890251629b71fc
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-01 11:22:02 -0400

    trying to refactor rule4

--- src/rules/escalation/rule4.js
+++ src/rules/escalation/rule4.js
@@ -5,64 +5,122 @@ import * as categorize from '../library/categorizeDose';
 const rule4 = ( patientMedications, masterMedications ) => {
   return _.chain( patientMedications )
       .reduce( ( result, medication ) => {
-        const rule = _.partial( ( medicationElement, patientMedication ) => {
+        const rule = _.partial( ( medicationElement, originalMedications, patientMedication ) => {
           if ( patientMedication.chemicalType === 'ICS' &&
             patientMedication.name !== 'symbicort' &&
             ( categorize.patientICSDose( patientMedication ) === 'medium' ||
             categorize.patientICSDose( patientMedication ) === 'high' ) &&
             ( !_.isEmpty( _.filter( patientMedications, { chemicalType: 'laba' } ) ) ) ) {
-            if ( !_.isEmpty( _.filter( patientMedications, { chemicalType: 'laba, ICS' } ) ) ) {
-              result.push( patientMedication );
-              result.push( _.filter( medicationElement, { name: 'singulair' } ) );
-            }
-            const getLABAAndICS = _.filter( patientMedications,
-              ( medication ) => {
-                return medication.chemicalType === 'laba' || medication.chemicalType === 'ICS';
-              } );
-            if ( !_.isEmpty( getLABAAndICS ) ) {
-              const filteredMedication = _.filter( medicationElement,
-                {
-                  chemicalType: 'laba,ICS',
-                  chemicalABA: patientMedication.chemicalLABA,
-                  chemicalICS: patientMedication.chemicalICS,
-                } );
-              // same as rule 3 and 5 chemical LABA and chemicalICS clarification
-              if ( !_.isEmpty( filteredMedication ) ) {
-                if ( !_.isEmpty( _.filter( filteredMedication, { device: patientMedication.device } ) ) ) {
-                  // console.log("device");
-                  if ( !_.isEmpty( _.filter( filteredMedication, ( medication ) => {
-                    return medication.device === patientMedication.device &&
-                       calculate.ICSDose( medication ) === calculate.patientICSDose( patientMedication );
-                  } ) ) ) {
-                    result.push( _.max(
-                      _.filter( filteredMedication, { device: patientMedication.device } ), 'doseICS' ) );
-                  }
-                  else {
-                    result.push( _.filter( filteredMedication, ( medication ) => {
-                      return medication.device === patientMedication.device &&
-                        calculate.ICSDose( medication ) === calculate.patientICSDose( patientMedication );
-                    } ) );
-                  }
-                  result.push( patientMedication );
-                  result.push( _.filter( medicationElement, { name: 'singulair' } ) );
+
+            result.push( _.chain( originalMedications )
+              .filter( { chemicalType: 'laba,ICS' } )
+              .concat( patientMedication )
+              .concat(
+                _.chain( medicationElement )
+                .filter( { name: 'singular' } )
+                .value(),
+              )
+              .value(),
+            );
+
+            result.push( _
+              .chain( medicationElement )
+              .reduce( ( accResult, medication ) => {
+                const laba = _.chain( originalMedications )
+                  .find( { chemicalType: 'laba' } )
+                  .value();
+                if ( medication.name === 'singulair' ) {
+                  accResult.push( medication );
                 }
-                else {
-                  result.push( patientMedication );
-                  result.push( _.filter( medicationElement, { name: 'singulair' } ) );
+
+                if ( medication.chemicalType !== 'laba,ICS' &&
+                  medication.chemicalLABA !== patientMedication.chemicalLABA &&
+                  medication.chemicalICS !== patientMedication.chemicalICS &&
+                  ( medication.device !== patientMedication.device &&
+                    medication.device !== laba.device
+                  ) &&
+                  calculate.ICSDose( medication ) !== calculate.patientICSDose( patientMedication )
+                ) {
+                  accResult.push( patientMedication );
+
+                  return accResult;
                 }
-              }
-              else {
-                result.push( patientMedication );
-                result.push( _.filter( medicationElement, { name: 'singulair' } ) );
-              }
-            }
+
+                if ( medication.device === patientMedication.device && ( _.isNil( accResult.new ) ||
+                     calculate.ICSDose( accResult.new ) <= calculate.ICSDose( medication ) )
+                   ) {
+                  accResult.new = medication;
+
+                  return accResult;
+                }
+                else if ( medication.device === laba.device && ( _.isNil( accResult.new ) ||
+                    calculate.ICSDose( accResult.new ) <= calculate.ICSDose( medication ) )
+                ) {
+                  accResult.new = medication;
+
+                  return accResult;
+                }
+
+                return accResult;
+              }, [] )
+              .thru( ( medication ) => {
+                return medication.new[0] || medication;
+              } )
+              .value(),
+            );
+
+            // if ( !_.isEmpty( _.filter( patientMedications, { chemicalType: 'laba, ICS' } ) ) ) {
+            //   result.push( patientMedication );
+            //   result.push( _.filter( medicationElement, { name: 'singulair' } ) );
+            // }
+            // const getLABAAndICS = _.filter( patientMedications,
+            //   ( medication ) => {
+            //     return medication.chemicalType === 'laba' || medication.chemicalType === 'ICS';
+            //   } );
+            // if ( !_.isEmpty( getLABAAndICS ) ) {
+            //   const filteredMedication = _.filter( medicationElement,
+            //     {
+            //       chemicalType: 'laba,ICS',
+            //       chemicalABA: patientMedication.chemicalLABA,
+            //       chemicalICS: patientMedication.chemicalICS,
+            //     } );
+            //   // same as rule 3 and 5 chemical LABA and chemicalICS clarification
+            //   if ( !_.isEmpty( filteredMedication ) ) {
+            //     if ( !_.isEmpty( _.filter( filteredMedication, { device: patientMedication.device } ) ) ) {
+            //       // console.log("device");
+            //       if ( !_.isEmpty( _.filter( filteredMedication, ( medication ) => {
+            //         return medication.device === patientMedication.device &&
+            //            calculate.ICSDose( medication ) === calculate.patientICSDose( patientMedication );
+            //       } ) ) ) {
+            //         result.push( _.max(
+            //           _.filter( filteredMedication, { device: patientMedication.device } ), 'doseICS' ) );
+            //       }
+            //       else {
+            //         result.push( _.filter( filteredMedication, ( medication ) => {
+            //           return medication.device === patientMedication.device &&
+            //             calculate.ICSDose( medication ) === calculate.patientICSDose( patientMedication );
+            //         } ) );
+            //       }
+            //       result.push( patientMedication );
+            //       result.push( _.filter( medicationElement, { name: 'singulair' } ) );
+            //     }
+            //     else {
+            //       result.push( patientMedication );
+            //       result.push( _.filter( medicationElement, { name: 'singulair' } ) );
+            //     }
+            //   }
+            //   else {
+            //     result.push( patientMedication );
+            //     result.push( _.filter( medicationElement, { name: 'singulair' } ) );
+            //   }
+            // }
           }
           if ( patientMedication.name === 'symbicort' &&
             ( categorize.patientICSDose( patientMedication ) === 'medium' ||
             categorize.patientICSDose( patientMedication ) === 'high' ) ) {
             result.push( _.filter( medicationElement, { name: 'symbicort', din: patientMedication.din } ) );
           }
-        }, masterMedications );
+        }, masterMedications, patientMedications );
         rule( medication );
 
         return result;
