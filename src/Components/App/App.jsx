e792e13e967b5562300b577a32a3170ef34c01f9
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-03 11:01:09 -0400

    created questionnaire. need to format and add functionality

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -6,6 +6,7 @@ import MedicationTable from '../MedicationTable/MedicationTable';
 import medicationData from '../../medicationData/medicationData';
 import DisplayPatientMedications from '../Display/PatientMedications/PatientMedications';
 import Recommendations from '../Display/Recommendations/Recommendations';
+import Questionnaire from '../Questionnaire/Questionnaire';
 import * as get from '../../rules/escalation/rules';
 import './styles.css';
 
@@ -78,6 +79,9 @@ saveRecommendation,
       <div className="app__header">
         <Header />
       </div>
+    <div>
+      <Questionnaire />
+    </div>
       <div className="app__main">
         <MedicationTable
           onChangeMedication={onMedicationSelection}
--- src/Components/Questionnaire/Questionnaire.js
+++ src/Components/Questionnaire/Questionnaire.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import _ from 'lodash';
-import PropTypes from 'prop-types';
+// import _ from 'lodash';
+// import PropTypes from 'prop-types';
 import './styles.css';
 
 const Questionnaire = ( ) => {
@@ -18,25 +18,109 @@ past 3 months)”
 AND/OR
 “There has been a time when the patient was exercising and had to stop because of
 asthma symptoms (in the past 3 months)”
+
+              checked={this.state.selectedOption === 'option2'}
+              onChange={this.handleOptionChange}/>
  */
+  const options = () => {
+    return (
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="one"
+          />
+          None
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="one"
+          />
+          1
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="two"
+          />
+          2
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="three"
+          />
+          3
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="four"
+          />
+          4
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="more"
+          />
+          4+
+        </label>
+      </div>
+    );
+  }
+  const yesNo = () => {
+    return (
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="no"
+          />
+          No
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="yes"
+          />
+          Yes
+        </label>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <div className='wakeUp'>
-        <p>On average, how many times each week have you been woken up?</p>
-      </div>
-      <div className='asthmaSymptoms'>
-        <p>On average, how many times each week have you had asthma symptoms?</p>
-      </div>
-      <div className='rescuePuffer'>
-        <p>On average, how many times each week do you need to use a rescue puffer?</p>
-      </div>
-      <div className='missedEvent'>
-        <p>Have you missed, work, school, and/or other activites because of asthma (in the past 3 months)?</p>
-      </div>
-      <div className='wakeUp'>
-        <p>Has there been a time when you were exercising and had to stop because of asthma symptoms (in the last 3 months)?</p>
-      </div>
+      <h3>Asthma Control Questionnaire</h3>
+      <form onSubmit=''>
+        <div className='wakeUp'>
+          <p>On average, how many times each week have you been woken up?</p>
+          <div className='options'>
+            {options()}
+          </div>
+        </div>
+        <div className='asthmaSymptoms'>
+          <p>On average, how many times each week have you had asthma symptoms?</p>
+          <div className='options'>
+            {options()}
+          </div>
+        </div>
+        <div className='rescuePuffer'>
+          <p>On average, how many times each week do you need to use a rescue puffer?</p>
+          {options()}
+        </div>
+        <div className='missedEvent'>
+          <p>Have you missed, work, school, and/or other activites because of asthma (in the past 3 months)?</p>
+          {yesNo()}
+        </div>
+        <div className='wakeUp'>
+          <p>Has there been a time when you were exercising and had to stop because of asthma symptoms (in the last 3 months)?</p>
+          {yesNo()}
+        </div>
+        <button className='buttom' type='submit'>Submit</button>
+      </form>
     </div>
   );
 };
