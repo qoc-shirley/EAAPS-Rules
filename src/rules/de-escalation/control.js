ab4bb872eca39630240ce35ecb6e4f015fa2dfb9
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-03 14:08:53 -0400

    edited naming for last questionnaire aswer

--- src/rules/de-escalation/control.js
+++ src/rules/de-escalation/control.js
@@ -1,19 +1,21 @@
 import _ from 'lodash';
 
 const control = ( data ) => {
-  return _.chain( data )
+  const isGood = _.chain( data )
     .thru( ( questionData ) => {
       if ( questionData[0].wakeUp !== '0' &&
         questionData[0].asthmaSymptoms !== '0' &&
         questionData[0].rescuePuffer !== '0' &&
         questionData[0].missedEvent !== 'no' &&
-        questionData[0].missedExercising !== 'no' ) {
+        questionData[0].stoppedExercising !== 'no' ) {
         return 'good control';
       }
       return 'not good control';
     } )
     .thru( control => control )
     .value();
+  console.log("control: ", isGood);
+  return isGood;
 };
 
 export default control;
