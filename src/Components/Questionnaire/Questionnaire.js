b6a11b60072827d18b9e9825073303cc48edb1bc
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-03 10:25:32 -0400

    implementing questionnaire to determine if patient has good control

---
+++ src/Components/Questionnaire/Questionnaire.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+const Questionnaire = ( ) => {
+  
+};
+
+export default Questionnaire;
