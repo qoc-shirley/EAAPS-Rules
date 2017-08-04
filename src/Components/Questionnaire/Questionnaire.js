c4ce6846df5f03d28afd9edc21b1c7b19dfbe7d1
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-04 09:55:40 -0400

    fixed eslint erros for questionnaire component

--- src/Components/Questionnaire/Questionnaire.js
+++ src/Components/Questionnaire/Questionnaire.js
@@ -5,65 +5,64 @@ import { connect } from 'react-redux';
 import * as actions from '../../redux/App/actions';
 import './styles.css';
 
-const Questionnaire = ( { onChangeQuestionnaireSelect, medication }) => {
-
+const Questionnaire = ( { onChangeQuestionnaireSelect, medication } ) => {
   return (
     <div className="questionnaire">
-      <form className= "questions" onSubmit=''>
+      <form className="questions" onSubmit="">
         <h3>Asthma Control Questionnaire</h3>
         <div className="wakeUp">
           <p>On average, how many times each week have you been woken up?</p>
           <div className="selectOption">
-            <label>
+            <label htmlFor="wakeUpOption0">
               <input
                 type="radio"
                 value="0"
-                checked={medication.wakeUp === "0"}
+                checked={medication.wakeUp === '0'}
                 onChange={() => onChangeQuestionnaireSelect( 'wakeUp', '0' )}
               />
               0
             </label>
-            <label>
+            <label htmlFor="wakeUpOption1">
               <input
                 type="radio"
                 value="1"
-                checked={medication.wakeUp === "1"}
+                checked={medication.wakeUp === '1'}
                 onChange={() => onChangeQuestionnaireSelect( 'wakeUp', '1' )}
               />
               1
             </label>
-            <label>
+            <label htmlFor="wakeUpOption2">
               <input
                 type="radio"
                 value="2"
-                checked={medication.wakeUp === "2"}
+                checked={medication.wakeUp === '2'}
                 onChange={() => onChangeQuestionnaireSelect( 'wakeUp', '2' )}
               />
               2
             </label>
-            <label>
+            <label htmlFor="wakeUpOption3">
               <input
                 type="radio"
                 value="3"
-                checked={medication.wakeUp === "3"}
+                checked={medication.wakeUp === '3'}
                 onChange={() => onChangeQuestionnaireSelect( 'wakeUp', '3' )}
               />
               3
             </label>
-            <label>
+            <label htmlFor="wakeUpOption4">
               <input
                 type="radio"
                 value="4"
-                checked={medication.wakeUp === "4"}
+                checked={medication.wakeUp === '4'}
                 onChange={() => onChangeQuestionnaireSelect( 'wakeUp', '4' )}
               />
               4
             </label>
-            <label>
+            <label htmlFor="wakeUpOptionMore">
               <input
                 type="radio"
                 value="more"
-                checked={medication.wakeUp === "more"}
+                checked={medication.wakeUp === 'more'}
                 onChange={() => onChangeQuestionnaireSelect( 'wakeUp', 'more' )}
               />
               4+
@@ -72,57 +71,57 @@ const Questionnaire = ( { onChangeQuestionnaireSelect, medication }) => {
         </div>
         <div className="asthmaSymptoms">
           <p>On average, how many times each week have you had asthma symptoms?</p>
-          <div className='selectOption'>
-            <label>
+          <div className="selectOption">
+            <label htmlFor="asthmaSymptomsOption0">
               <input
                 type="radio"
                 value="0"
-                checked={medication.asthmaSymptoms === "0"}
+                checked={medication.asthmaSymptoms === '0'}
                 onChange={() => onChangeQuestionnaireSelect( 'asthmaSymptoms', '0' )}
               />
               0
             </label>
-            <label>
+            <label htmlFor="asthmaSymptomsOption1">
               <input
                 type="radio"
                 value="1"
-                checked={medication.asthmaSymptoms === "1"}
+                checked={medication.asthmaSymptoms === '1'}
                 onChange={() => onChangeQuestionnaireSelect( 'asthmaSymptoms', '1' )}
               />
               1
             </label>
-            <label>
+            <label htmlFor="asthmaSymptomsOption2">
               <input
                 type="radio"
                 value="2"
-                checked={medication.asthmaSymptoms === "2"}
+                checked={medication.asthmaSymptoms === '2'}
                 onChange={() => onChangeQuestionnaireSelect( 'asthmaSymptoms', '2' )}
               />
               2
             </label>
-            <label>
+            <label htmlFor="asthmaSymptomsOption3">
               <input
                 type="radio"
                 value="3"
-                checked={medication.asthmaSymptoms === "3"}
+                checked={medication.asthmaSymptoms === '3'}
                 onChange={() => onChangeQuestionnaireSelect( 'asthmaSymptoms', '3' )}
               />
               3
             </label>
-            <label>
+            <label htmlFor="asthmaSymptomsOption4">
               <input
                 type="radio"
                 value="4"
-                checked={medication.asthmaSymptoms === "4"}
+                checked={medication.asthmaSymptoms === '4'}
                 onChange={() => onChangeQuestionnaireSelect( 'asthmaSymptoms', '4' )}
               />
               4
             </label>
-            <label>
+            <label htmlFor="asthmaSymptomsOptionMore">
               <input
                 type="radio"
                 value="more"
-                checked={medication.asthmaSymptoms === "more"}
+                checked={medication.asthmaSymptoms === 'more'}
                 onChange={() => onChangeQuestionnaireSelect( 'asthmaSymptoms', 'more' )}
               />
               4+
@@ -132,56 +131,56 @@ const Questionnaire = ( { onChangeQuestionnaireSelect, medication }) => {
         <div className="rescuePuffer">
           <p>On average, how many times each week do you need to use a rescue puffer?</p>
           <div className="selectOption">
-            <label>
+            <label htmlFor="rescuePufferOption0">
               <input
                 type="radio"
                 value="0"
-                checked={medication.rescuePuffer === "0"}
+                checked={medication.rescuePuffer === '0'}
                 onChange={() => onChangeQuestionnaireSelect( 'rescuePuffer', '0' )}
               />
               0
             </label>
-            <label>
+            <label htmlFor="rescuePufferOption1">
               <input
                 type="radio"
                 value="1"
-                checked={medication.rescuePuffer === "1"}
+                checked={medication.rescuePuffer === '1'}
                 onChange={() => onChangeQuestionnaireSelect( 'rescuePuffer', '1' )}
               />
               1
             </label>
-            <label>
+            <label htmlFor="rescuePufferOption2">
               <input
                 type="radio"
                 value="2"
-                checked={medication.rescuePuffer === "2"}
+                checked={medication.rescuePuffer === '2'}
                 onChange={() => onChangeQuestionnaireSelect( 'rescuePuffer', '2' )}
               />
               2
             </label>
-            <label>
+            <label htmlFor="rescuePufferOption3">
               <input
                 type="radio"
                 value="3"
-                checked={medication.rescuePuffer === "3"}
+                checked={medication.rescuePuffer === '3'}
                 onChange={() => onChangeQuestionnaireSelect( 'rescuePuffer', '3' )}
               />
               3
             </label>
-            <label>
+            <label htmlFor="rescuePufferOption4">
               <input
                 type="radio"
                 value="4"
-                checked={medication.rescuePuffer === "4"}
+                checked={medication.rescuePuffer === '4'}
                 onChange={() => onChangeQuestionnaireSelect( 'rescuePuffer', '4' )}
               />
               4
             </label>
-            <label>
+            <label htmlFor="rescuePufferOptionMore">
               <input
                 type="radio"
                 value="more"
-                checked={medication.rescuePuffer === "more"}
+                checked={medication.rescuePuffer === 'more'}
                 onChange={() => onChangeQuestionnaireSelect( 'rescuePuffer', 'more' )}
               />
               4+
@@ -191,20 +190,20 @@ const Questionnaire = ( { onChangeQuestionnaireSelect, medication }) => {
         <div className="missedEvent">
           <p>Have you missed, work, school, and/or other activites because of asthma (in the past 3 months)?</p>
           <div className="selectOption">
-            <label>
+            <label htmlFor="missedEventOptionNo">
               <input
                 type="radio"
                 value="no"
-                checked={medication.missedEvent === "no"}
+                checked={medication.missedEvent === 'no'}
                 onChange={() => onChangeQuestionnaireSelect( 'missedEvent', 'no' )}
               />
               No
             </label>
-            <label>
+            <label htmlFor="missedEventOptionYes">
               <input
                 type="radio"
                 value="yes"
-                checked={medication.missedEvent === "yes"}
+                checked={medication.missedEvent === 'yes'}
                 onChange={() => onChangeQuestionnaireSelect( 'missedEvent', 'yes' )}
               />
               Yes
@@ -212,22 +211,23 @@ const Questionnaire = ( { onChangeQuestionnaireSelect, medication }) => {
           </div>
         </div>
         <div className="stoppedExercising">
-          <p>Has there been a time when you were exercising and had to stop because of asthma symptoms (in the last 3 months)?</p>
+          <p>Has there been a time when you were exercising and had to stop because of asthma symptoms
+            (in the last 3 months)?</p>
           <div className="selectOption">
-            <label>
+            <label htmlFor="stoppedExercisingOptionNo">
               <input
                 type="radio"
                 value="no"
-                checked={medication.stoppedExercising === "no"}
+                checked={medication.stoppedExercising === 'no'}
                 onChange={() => onChangeQuestionnaireSelect( 'stoppedExercising', 'no' )}
               />
               No
             </label>
-            <label>
+            <label htmlFor="stoppedExercisingOptionYes">
               <input
                 type="radio"
                 value="yes"
-                checked={medication.stoppedExercising === "yes"}
+                checked={medication.stoppedExercising === 'yes'}
                 onChange={() => onChangeQuestionnaireSelect( 'stoppedExercising', 'yes' )}
               />
               Yes
@@ -244,8 +244,8 @@ const mapStateToProps = state => ( {
 } );
 
 const mapDispatchToProps = dispatch => ( {
-  onChangeQuestionnaireSelect: ( question, option ) => dispatch( actions.onChangeQuestionnaireSelect( question, option ) ),
-
+  onChangeQuestionnaireSelect:
+    ( question, option ) => dispatch( actions.onChangeQuestionnaireSelect( question, option ) ),
 } );
 
 export default connect( mapStateToProps, mapDispatchToProps )( Questionnaire );
