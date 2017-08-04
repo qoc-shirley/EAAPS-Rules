68c585fd807a0b683e1128b701af010a0885996c
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-04 16:18:40 -0400

    starting rule2

--- src/rules/de-escalation/rule2.js
+++ src/rules/de-escalation/rule2.js
@@ -1,8 +1,19 @@
+import _ from 'lodash';
 // import * as get from '../library/getICSDose';
 // import * as calculate from '../library/calculateICSDose';
 // import * as adjust from '../library/adjustICSDose';
 
 const rule2 = ( patientMedications, masterMedications ) => {
+  return _.chain( patientMedications )
+    .reduce( ( result, medication ) => {
+      const rule = _.partial( ( medicationElement, originalMedications, patientMedication ) => {
+
+      }, masterMedications, patientMedications );
+      rule( medication );
+
+      return result.push( rule );
+    }, [] )
+    .value();
 };
 
 export default rule2;
