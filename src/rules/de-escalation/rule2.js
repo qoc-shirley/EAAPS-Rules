9b0a8f908759e1d4db4248d700e8d550069676dd
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-04 15:47:05 -0400

    created empty function for rule2 and imported some files that might be used

---
+++ src/rules/de-escalation/rule2.js
@@ -0,0 +1,9 @@
+// import _ from 'lodash';
+// import * as get from '../library/getICSDose';
+// import * as calculate from '../library/calculateICSDose';
+// import * as adjust from '../library/adjustICSDose';
+
+const rule2 = ( patientMedications, masterMedications ) => {
+};
+
+export default rule2;
