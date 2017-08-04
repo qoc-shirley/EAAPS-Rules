5ff9e47457c2202e269adc16948edd01c2ccdea7
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-04 16:49:49 -0400

    creating helper function for totalDoseReduction

--- src/rules/de-escalation/rule3.js
+++ src/rules/de-escalation/rule3.js
@@ -113,60 +113,60 @@ const rule3 = ( patientMedications, masterMedications, questionnaireAnswers ) =>
               return result.push( fifty );
             }
             else if ( patientMedication.chemicalType === 'laba,ICS' ) {
-              const exactlyFifty = _.chain( sameChemicalLabaAndIcs )
-                .thru( ( medication ) => {
-                  return adjust.checkDoseReduction(
-                    medication,
-                    'exactlyFifty',
-                    calculate.patientICSDose( patientMedication ),
-                  );
-                } )
-                .filter( ( medication ) => {
-                  return medication.device === patientMedication.device;
-                } )
-                .value();
-
-              if ( _.isEmpty( exactlyFifty ) ) {
-                // adjust timesPerDay/DoseICS and prioritize puffsPerTime
-                const betweenFiftyAndFullDose = _.chain( sameChemicalLabaAndIcs )
-                  .thru( ( medication ) => {
-                    return adjust.checkDoseReduction(
-                      medication,
-                      'betweenFiftyAndFullDose',
-                      calculate.patientICSDose( patientMedication ),
-                    );
-                  } )
-                  .thru( get.lowestICSDose )
-                  .value();
-                if ( _.isEmpty( betweenFiftyAndFullDose ) ) {
-                  return _.chain( sameChemicalLabaAndIcs )
-                    .thru( ( medication ) => {
-                      if ( medication.timesPerDay === '1 OR 2' ) {
-                        if ( calculate.ICSDose( medication ) >= calculate.ICSDose( patientMedication ) / 2 &&
-                          calculate.ICSDose( medication ) < calculate.ICSDose( patientMedication )
-                        ) {
-                          return medication;
-                        }
-                        else if ( calculate.ICSDose( medication ) * 2 >= calculate.ICSDose( patientMedication ) / 2 &&
-                          calculate.ICSDose( medication ) * 2 < calculate.ICSDose( patientMedication ) ) {
-                          return medication;
-                        }
-                      }
-
-                      return adjust.checkDoseReduction(
-                        medication,
-                        'betweenFiftyAndFullDose',
-                        calculate.patientICSDose( patientMedication ),
-                      );
-                    } )
-                    .thru( get.lowestICSDose )
-                    .value();
-                }
-
-                return result.push( betweenFiftyAndFullDose );
-              }
-
-              return result.push( exactlyFifty );
+              // const exactlyFifty = _.chain( sameChemicalLabaAndIcs )
+              //   .thru( ( medication ) => {
+              //     return adjust.checkDoseReduction(
+              //       medication,
+              //       'exactlyFifty',
+              //       calculate.patientICSDose( patientMedication ),
+              //     );
+              //   } )
+              //   .filter( ( medication ) => {
+              //     return medication.device === patientMedication.device;
+              //   } )
+              //   .value();
+              //
+              // if ( _.isEmpty( exactlyFifty ) ) {
+              //   // adjust timesPerDay/DoseICS and prioritize puffsPerTime
+              //   const betweenFiftyAndFullDose = _.chain( sameChemicalLabaAndIcs )
+              //     .thru( ( medication ) => {
+              //       return adjust.checkDoseReduction(
+              //         medication,
+              //         'betweenFiftyAndFullDose',
+              //         calculate.patientICSDose( patientMedication ),
+              //       );
+              //     } )
+              //     .thru( get.lowestICSDose )
+              //     .value();
+              //   if ( _.isEmpty( betweenFiftyAndFullDose ) ) {
+              //     return _.chain( sameChemicalLabaAndIcs )
+              //       .thru( ( medication ) => {
+              //         if ( medication.timesPerDay === '1 OR 2' ) {
+              //           if ( calculate.ICSDose( medication ) >= calculate.ICSDose( patientMedication ) / 2 &&
+              //             calculate.ICSDose( medication ) < calculate.ICSDose( patientMedication )
+              //           ) {
+              //             return medication;
+              //           }
+              //           else if ( calculate.ICSDose( medication ) * 2 >= calculate.ICSDose( patientMedication ) / 2 &&
+              //             calculate.ICSDose( medication ) * 2 < calculate.ICSDose( patientMedication ) ) {
+              //             return medication;
+              //           }
+              //         }
+              //
+              //         return adjust.checkDoseReduction(
+              //           medication,
+              //           'betweenFiftyAndFullDose',
+              //           calculate.patientICSDose( patientMedication ),
+              //         );
+              //       } )
+              //       .thru( get.lowestICSDose )
+              //       .value();
+              //   }
+              //
+              //   return result.push( betweenFiftyAndFullDose );
+              // }
+              //
+              // return result.push( exactlyFifty );
             }
           }
           if ( onSMART ) {
---
+++ src/rules/library/totalDoseReduction.js
@@ -0,0 +1,63 @@
+import _ from 'lodash';
+import * as get from '../library/getICSDose';
+import * as calculate from '../library/calculateICSDose';
+import * as adjust from '../library/adjustICSDose';
+
+export const totalDoseReduction = ( patientMedication, filteredMedications ) => {
+  const exactlyFifty = _.chain( filteredMedications )
+    .thru( ( medication ) => {
+      return adjust.checkDoseReduction(
+        medication,
+        'exactlyFifty',
+        calculate.patientICSDose( patientMedication ),
+      );
+    } )
+    .filter( ( medication ) => {
+      return medication.device === patientMedication.device;
+    } )
+    .value();
+
+  if ( _.isEmpty( exactlyFifty ) ) {
+    // adjust timesPerDay/DoseICS and prioritize puffsPerTime
+    const betweenFiftyAndFullDose = _.chain( filteredMedications )
+      .thru( ( medication ) => {
+        return adjust.checkDoseReduction(
+          medication,
+          'betweenFiftyAndFullDose',
+          calculate.patientICSDose( patientMedication ),
+        );
+      } )
+      .thru( get.lowestICSDose )
+      .value();
+    if ( _.isEmpty( betweenFiftyAndFullDose ) ) {
+      return _.chain( filteredMedications )
+        .thru( ( medication ) => {
+          if ( medication.timesPerDay === '1 OR 2' ) {
+            if ( calculate.ICSDose( medication ) >= calculate.ICSDose( patientMedication ) / 2 &&
+              calculate.ICSDose( medication ) < calculate.ICSDose( patientMedication )
+            ) {
+              return medication;
+            }
+            else if ( calculate.ICSDose( medication ) * 2 >= calculate.ICSDose( patientMedication ) / 2 &&
+              calculate.ICSDose( medication ) * 2 < calculate.ICSDose( patientMedication ) ) {
+              return medication;
+            }
+          }
+
+          return adjust.checkDoseReduction(
+            medication,
+            'betweenFiftyAndFullDose',
+            calculate.patientICSDose( patientMedication ),
+          );
+        } )
+        .thru( get.lowestICSDose )
+        .value();
+    }
+
+    return betweenFiftyAndFullDose;
+  }
+
+  return exactlyFifty;
+};
+
+export default totalDoseReduction;
