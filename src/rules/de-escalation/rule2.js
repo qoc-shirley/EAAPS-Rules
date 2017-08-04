1361740a984e0564cfcd5c40aff424775aa15747
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-04 16:24:47 -0400

    added code to check if there is no laba or ltra

--- src/rules/de-escalation/rule2.js
+++ src/rules/de-escalation/rule2.js
@@ -1,13 +1,78 @@
 import _ from 'lodash';
 // import * as get from '../library/getICSDose';
-// import * as calculate from '../library/calculateICSDose';
+import * as calculate from '../library/calculateICSDose';
 // import * as adjust from '../library/adjustICSDose';
 
 const rule2 = ( patientMedications, masterMedications ) => {
   return _.chain( patientMedications )
     .reduce( ( result, medication ) => {
       const rule = _.partial( ( medicationElement, originalMedications, patientMedication ) => {
+        const compareLowestDose = _.chain( medicationElement )
+          .filter( ( findMedication ) => {
+            return (
+              findMedication.name === 'flovent' &&
+              findMedication.device === 'inhaler2' &&
+              findMedication.doseICS === '100'
+            ) || (
+              findMedication.name === 'flovent' &&
+              findMedication.device === 'diskus' &&
+              findMedication.doseICS === '200'
+            ) || (
+              findMedication.name === 'pulmicort' &&
+              findMedication.device === 'turbuhaler' &&
+              findMedication.doseICS === '200'
+            ) || (
+              findMedication.name === 'qvar' &&
+              findMedication.device === 'inhaler1' &&
+              findMedication.doseICS === '100'
+            ) || (
+              findMedication.name === 'asthmanex' &&
+              findMedication.device === 'twisthaler' &&
+              findMedication.doseICS === '100'
+            ) || (
+              findMedication.name === 'alvesco' &&
+              findMedication.device === 'inhaler1' &&
+              findMedication.doseICS === '200'
+            ) || (
+              findMedication.name === 'advair' &&
+              findMedication.device === 'inhaler2' &&
+              findMedication.doseICS === '250'
+            ) || (
+              findMedication.name === 'advair' &&
+              findMedication.device === 'diskus' &&
+              findMedication.doseICS === '200'
+            ) || (
+              findMedication.name === 'symbicort' &&
+              findMedication.device === 'turbuhaler' &&
+              findMedication.doseICS === '200'
+            ) || (
+              findMedication.name === 'zenhale' &&
+              findMedication.device === 'inhaler2' &&
+              findMedication.doseICS === '200'
+            ) || (
+              findMedication.name === 'arnuity' &&
+              findMedication.device === 'inhaler2' &&
+              findMedication.doseICS === '100'
+            ) || (
+              findMedication.name === 'breo' &&
+              findMedication.device === 'ellipta' &&
+              findMedication.doseICS === '100'
+            );
+          } )
+          .thru( get.lowestICSDose )
+          .value();
+        const noLabaLtra = _.chain( originalMedications )
+          .filter( ( medication ) => {
+            return medication.chemicalType === 'laba' || medication.chemicalType === 'ltra';
+          } );
 
+        if ( patientMedication === 'ICS' &&
+          _.isEmpty( noLabaLtra ) &&
+          calculate.patientICSDose( patientMedication ) > calculate.ICSDose( compareLowestDose ) ) {
+
+        }
+
+        return result;
       }, masterMedications, patientMedications );
       rule( medication );
 
