c9924a33010a588c82d6d3092786923beb4f1605
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-09 16:05:58 -0400

    edited deRule5: started working on rule5

--- src/rules/de-escalation/rule5.js
+++ src/rules/de-escalation/rule5.js
@@ -1,7 +1,47 @@
-// import _ from 'lodash';
-// import rule3 from './rule3';
+import _ from 'lodash';
+import rule3 from './rule3';
 
 const rule5 = ( patientMedications, masterMedications ) => {
+  return _.chain( patientMedications )
+    .reduce( ( result, medication ) => {
+      const rule = _.partial( ( medicationElement, originalMedications, patientMedication ) => {
+        const noLaba = _.chain( originalMedications )
+          .filter( ( medication ) => {
+            return medication.chemicalType === 'laba';
+          } )
+          .isEmpty()
+          .value();
+
+        const noLabaICS = _.chain( originalMedications )
+          .filter( ( medication ) => {
+            return medication.chemicalType === 'laba,ICS';
+          } )
+          .isEmpty()
+          .value();
+
+        const noLtra = _.chain( originalMedications )
+          .filter( ( medication ) => {
+            return medication.chemicalType === 'ltra';
+          } )
+          .isEmpty()
+          .value();
+
+        if ( patientMedication === 'ICS' && ( !noLaba || !noLabaICS ) && !noLtra ) {
+          // Provide a choice to discontinue the LTRA
+          result.concat( [
+            'discontinue Ltra and continue ICS and laba/laba,ICS: ', _.concat( patientMedication, noLaba, noLabaICS ),
+            'continue recommend: ', rule3(),
+            'continue ltra: ', noLtra,
+          ] );
+        }
+
+        return result;
+      }, masterMedications, patientMedications );
+      rule( medication );
+
+      return result.push( rule );
+    }, [] )
+    .value();
 };
 
 export default rule5;
