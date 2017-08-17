143644535d10c492323ac501c68e969ed3c65448
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-17 11:10:18 -0400

    implemented part of code review for rule11, have questions for kent

--- src/rules/escalation/rule11.js
+++ src/rules/escalation/rule11.js
@@ -1,21 +1,30 @@
 import _ from 'lodash';
 import masterMedications from '../../medicationData/medicationData';
 
+// const getLabaICSAndICS = ( patientMedications ) => {
+//   const result = [];
+//
+//   return _.chain( patientMedications )
+//     .filter(
+//       _.partial( ( medicationElements, patientMedication ) => {
+//         if ( patientMedication.chemicalType === 'ICS' ) {
+//           result.push( patientMedication );
+//         }
+//         else if ( patientMedication.chemicalType === 'laba,ICS' ) {
+//           result.push( patientMedication );
+//         }
+//       }, masterMedications ) )
+//     .concat( result )
+//     .flatten()
+//     .value();
+// };
 const getLabaICSAndICS = ( patientMedications ) => {
-  const result = [];
-
   return _.chain( patientMedications )
     .filter(
       _.partial( ( medicationElements, patientMedication ) => {
-        if ( patientMedication.chemicalType === 'ICS' ) {
-          result.push( patientMedication );
-        }
-        else if ( patientMedication.chemicalType === 'laba,ICS' ) {
-          result.push( patientMedication );
-        }
+        return patientMedication.chemicalType === 'ICS' ||
+          patientMedication.chemicalType === 'laba,ICS';
       }, masterMedications ) )
-    .concat( result )
-    .flatten()
     .value();
 };
 
@@ -33,5 +42,21 @@ const rule11 = ( patientMedications, masterMedications ) => {
 
   return _.concat( newMedication, filteredPatientMedications );
 };
+
+//
+// const rule11 = ( patientMedications, masterMedications ) => {
+//   return _.chain(patientMedications)
+//     .thru(getLabaICSAndICS) //why do thru twice? how to check if both ICS and laba,ICS exists?
+//     .thru((medications) => {
+//       if (_.size(medications)) {
+//         return _.chain(masterMedications)
+//           .filter({ name: 'singulair' })
+//           .concat(medications)
+//           .value();
+//       }
+//
+//       return medications;
+//     })
+//     .value();
 
 export default rule11;
