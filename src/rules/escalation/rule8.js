44b89b048d2d95cb19a0ae2419d1d4a764b10e37
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-17 12:01:47 -0400

    extracted helper function out of rule 8

--- src/rules/escalation/rule8.js
+++ src/rules/escalation/rule8.js
@@ -17,41 +17,29 @@ const isSMARTMediumOrHigh = ( patientMedications ) => {
   .value();
 };
 
-const rule8 = ( patientMedications, masterMedications ) => {
-  // const isSMARTMediumOrHigh = _.chain( patientMedications )
-  //   .filter( ( patientMedication ) => {
-  //     if ( patientMedication.name === 'symbicort' &&
-  //       patientMedication.function === 'controller,reliever' &&
-  //       ( categorize.patientICSDose( patientMedication ) === 'medium' ||
-  //       categorize.patientICSDose( patientMedication ) === 'high' ) ) {
-  //       return true;
-  //     }
-  //
-  //     return false;
-  //   } )
-  //   .value();
-
-  if ( !_.isEmpty( isSMARTMediumOrHigh ) ) {
-    return isSMARTMediumOrHigh.concat( _.filter( masterMedications, { name: 'singulair' } ) );
-  }
-
-  return [];
-};
-
 // const rule8 = ( patientMedications, masterMedications ) => {
-//   return _.chain(patientMedications)
-//     .thru(isSMARTMediumOrHigh) //why are there 2 thru
-//     .thru(smartMedHighMeds => {
-//       if (!_.size(smartMedHighMeds)) {
-//         return [];
-//       }
+//   const smartMedication = isSMARTMediumOrHigh( patientMedications );
+//   if ( !_.isEmpty( smartMedication ) ) {
+//     return smartMedication.concat( _.filter( masterMedications, { name: 'singulair' } ) );
+//   }
 //
-//       return _.chain(masterMedications)
-//         .filter({ name: 'singulair' })
-//         .concat(smartMedHighMeds)
-//         .value();
-//     })
-//     .value();
+//   return [];
 // };
+
+const rule8 = ( patientMedications, masterMedications ) => {
+  return _.chain( patientMedications )
+    .thru( isSMARTMediumOrHigh )
+    .thru( smartMedHighMeds => {
+      if ( !_.size( smartMedHighMeds ) ) {
+        return [];
+      }
+
+      return _.chain( masterMedications )
+        .filter( { name: 'singulair' } )
+        .concat( smartMedHighMeds )
+        .value();
+    })
+    .value();
+};
 
 export default rule8;
