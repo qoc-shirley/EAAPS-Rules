4373b7c0d3572c702fd379ff5733931b4547d840
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-17 11:54:43 -0400

    changed rule 11 back

--- src/rules/escalation/rule11.js
+++ src/rules/escalation/rule11.js
@@ -1,30 +1,21 @@
 import _ from 'lodash';
 import masterMedications from '../../medicationData/medicationData';
 
-// const getLabaICSAndICS = ( patientMedications ) => {
-//   const result = [];
-//
-//   return _.chain( patientMedications )
-//     .filter(
-//       _.partial( ( medicationElements, patientMedication ) => {
-//         if ( patientMedication.chemicalType === 'ICS' ) {
-//           result.push( patientMedication );
-//         }
-//         else if ( patientMedication.chemicalType === 'laba,ICS' ) {
-//           result.push( patientMedication );
-//         }
-//       }, masterMedications ) )
-//     .concat( result )
-//     .flatten()
-//     .value();
-// };
 const getLabaICSAndICS = ( patientMedications ) => {
+  const result = [];
+
   return _.chain( patientMedications )
     .filter(
       _.partial( ( medicationElements, patientMedication ) => {
-        return patientMedication.chemicalType === 'ICS' ||
-          patientMedication.chemicalType === 'laba,ICS';
+        if ( patientMedication.chemicalType === 'ICS' ) {
+          result.push( patientMedication );
+        }
+        else if ( patientMedication.chemicalType === 'laba,ICS' ) {
+          result.push( patientMedication );
+        }
       }, masterMedications ) )
+    .concat( result )
+    .flatten()
     .value();
 };
 
@@ -42,21 +33,5 @@ const rule11 = ( patientMedications, masterMedications ) => {
 
   return _.concat( newMedication, filteredPatientMedications );
 };
-
-//
-// const rule11 = ( patientMedications, masterMedications ) => {
-//   return _.chain(patientMedications)
-//     .thru(getLabaICSAndICS) //why do thru twice? how to check if both ICS and laba,ICS exists?
-//     .thru((medications) => {
-//       if (_.size(medications)) {
-//         return _.chain(masterMedications)
-//           .filter({ name: 'singulair' })
-//           .concat(medications)
-//           .value();
-//       }
-//
-//       return medications;
-//     })
-//     .value();
 
 export default rule11;
