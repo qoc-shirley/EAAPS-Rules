a2b15b64316eb7121cc4436c06cb3d6864926d4d
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-17 11:23:28 -0400

    added code change comments to rule 8

--- src/rules/escalation/rule8.js
+++ src/rules/escalation/rule8.js
@@ -1,19 +1,35 @@
 import _ from 'lodash';
 import * as categorize from '../library/categorizeDose';
 
-const rule8 = ( patientMedications, masterMedications ) => {
-  const isSMARTMediumOrHigh = _.chain( patientMedications )
-    .filter( ( patientMedication ) => {
-      if ( patientMedication.name === 'symbicort' &&
-        patientMedication.function === 'controller,reliever' &&
-        ( categorize.patientICSDose( patientMedication ) === 'medium' ||
-        categorize.patientICSDose( patientMedication ) === 'high' ) ) {
-        return true;
-      }
+const isSMARTMediumOrHigh = ( patientMedications ) => {
+  return _.chain( patientMedications )
+  .filter( ( patientMedication ) => {
+    const icsDose = categorize.patientICSDose( patientMedication );
 
-      return false;
-    } )
-    .value();
+    if ( patientMedication.name === 'symbicort' &&
+      patientMedication.function === 'controller,reliever' &&
+      ( icsDose === 'medium' || icsDose === 'high' )) {
+      return true;
+    }
+
+    return false;
+  } )
+  .value();
+};
+
+const rule8 = ( patientMedications, masterMedications ) => {
+  // const isSMARTMediumOrHigh = _.chain( patientMedications )
+  //   .filter( ( patientMedication ) => {
+  //     if ( patientMedication.name === 'symbicort' &&
+  //       patientMedication.function === 'controller,reliever' &&
+  //       ( categorize.patientICSDose( patientMedication ) === 'medium' ||
+  //       categorize.patientICSDose( patientMedication ) === 'high' ) ) {
+  //       return true;
+  //     }
+  //
+  //     return false;
+  //   } )
+  //   .value();
 
   if ( !_.isEmpty( isSMARTMediumOrHigh ) ) {
     return isSMARTMediumOrHigh.concat( _.filter( masterMedications, { name: 'singulair' } ) );
@@ -21,5 +37,21 @@ const rule8 = ( patientMedications, masterMedications ) => {
 
   return [];
 };
+
+// const rule8 = ( patientMedications, masterMedications ) => {
+//   return _.chain(patientMedications)
+//     .thru(isSMARTMediumOrHigh) //why are there 2 thru
+//     .thru(smartMedHighMeds => {
+//       if (!_.size(smartMedHighMeds)) {
+//         return [];
+//       }
+//
+//       return _.chain(masterMedications)
+//         .filter({ name: 'singulair' })
+//         .concat(smartMedHighMeds)
+//         .value();
+//     })
+//     .value();
+// };
 
 export default rule8;
