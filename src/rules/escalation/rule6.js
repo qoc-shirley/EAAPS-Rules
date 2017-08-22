39390920d7fcfc0b613c2b60506bc68a4c983f8b
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-22 11:16:41 -0400

    deleted console.log

--- src/rules/escalation/rule6.js
+++ src/rules/escalation/rule6.js
@@ -8,17 +8,12 @@ const rule6 = ( patientMedications ) => {
       const filterChemicalTypeLtra = _.filter( patientMedications, { chemicalType: 'ltra' } );
       const filterChemicalTypeLaba = _.filter( patientMedications, { chemicalType: 'laba' } );
 
-      console.log('patientMedication type: ', patientMedication.chemicalType);
-      console.log('filterChemicalTypeLaba: ', filterChemicalTypeLaba);
-      console.log('filterChemicalTypeLtra: ', filterChemicalTypeLtra);
-      console.log('compare ICSDose: ',  calculate.patientICSDose( patientMedication ) , _.toInteger( patientMedication.maxGreenICS ));
       if ( patientMedication.name !== 'symbicort' &&
         ( patientMedication.chemicalType === 'laba,ICS' ||
         ( !_.isEmpty( filterChemicalTypeLaba ) && patientMedication.chemicalType === 'ICS' )
         ) &&
         calculate.patientICSDose( patientMedication ) >= _.toInteger( patientMedication.maxGreenICS ) &&
         !_.isEmpty( filterChemicalTypeLtra ) ) {
-        console.log('true');
         return true;
       }
 
