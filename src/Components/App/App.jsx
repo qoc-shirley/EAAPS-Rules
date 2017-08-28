41d59a6d5c08603c1243fe819202e6dd98880ca3
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-28 10:58:48 -0400

    hid de-escalation material

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -95,12 +95,12 @@ saveRecommendation,
           >
             Escalation
           </button>
-          <button
-            className="button__runRules"
-            onClick={() => deescalationRules()}
-          >
-            De-escalation
-          </button>
+          {/*<button*/}
+            {/*className="button__runRules"*/}
+            {/*onClick={() => deescalationRules()}*/}
+          {/*>*/}
+            {/*De-escalation*/}
+          {/*</button>*/}
           <input
             className="clear"
             type="submit"
@@ -133,7 +133,7 @@ saveRecommendation,
         <Header />
       </div>
       <div className="app__main">
-        <Questionnaire />
+        {/*<Questionnaire />*/}
         <MedicationTable
           onChangeMedication={onMedicationSelection}
           onChangePuffValue={onChangePuffValue}
