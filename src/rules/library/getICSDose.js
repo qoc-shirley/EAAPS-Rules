cd51c44a24aca62905972db5d11836f9e78ec75b
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-28 16:52:27 -0400

    fixed helper lowest/highest icsdose

--- src/rules/library/getICSDose.js
+++ src/rules/library/getICSDose.js
@@ -1,22 +1,14 @@
 import _ from 'lodash';
 import * as calculate from './calculateICSDose';
 
-export const lowestICSDose = (newMedications) => {
-  return _.chain(newMedications)
-      .minBy(newMedications,
-      (medication) => {
-        return calculate.ICSDose(medication);
-      },
+export const lowestICSDose = newMedications => _.chain( newMedications )
+      .minBy( newMedications,
+      medication => calculate.ICSDose( medication ),
     )
     .value();
-};
 
-export const highestICSDose = (newMedications) => {
-  return _.chain(newMedications)
+export const highestICSDose = newMedications => _.chain( newMedications )
     .maxBy(
-      (medication) => {
-        return calculate.ICSDose(medication);
-      },
+      medication => calculate.ICSDose( medication ),
     )
     .value();
-};
