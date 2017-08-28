79a4b92a5847ccf7e5fee59f8417149fe92b252b
Shirley Xiao <shirley.xiao@qochealth.com>
2017-08-28 16:55:11 -0400

    fixed eslint for match helper function

--- src/rules/library/match.js
+++ src/rules/library/match.js
@@ -1,23 +1,17 @@
 import _ from 'lodash';
 
-export const device = ( medications, matchMedication ) => {
-  return _.filter( medications, { device: matchMedication.device } );
-};
+export const device = ( medications, matchMedication ) =>
+  _.filter( medications, { device: matchMedication.device } );
 
-export const timesPerDay = ( medications, matchMedication ) => {
-  return _.filter( medications, ( medication ) => {
-    return medication.timesPerDay === matchMedication.timesPerDay || medication.timesPerDay === '1 OR 2';
-  } );
-};
+export const timesPerDay = ( medications, matchMedication ) =>
+  _.filter( medications,
+      medication => medication.timesPerDay === matchMedication.timesPerDay || medication.timesPerDay === '1 OR 2' );
 
-export const doseICS = ( medications, matchMedication ) => {
-  return _.filter( medications, { doseICS: matchMedication.doseICS } );
-};
+export const doseICS = ( medications, matchMedication ) =>
+  _.filter( medications, { doseICS: matchMedication.doseICS } );
 
 export const minimizePuffsPerTime = ( medications, minimizeMedicationsPuffs ) => {
-  const minimize = _.filter( medications, ( medication ) => {
-    return medication.doseICS < minimizeMedicationsPuffs.doseICS;
-  } );
+  const minimize = _.filter( medications, medication => medication.doseICS < minimizeMedicationsPuffs.doseICS );
   if ( _.size( minimize ) > 1 ) {
     return _.maxBy( minimize, 'doseICS' );
   }
