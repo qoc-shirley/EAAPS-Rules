92c54cd92ade2c4a4c8c705d384d789081217866
Shirley Xiao <shirley.xiao@qochealth.com>
2017-09-05 15:59:40 -0400

    edited es rule 0: return recommendation when patient is on no medication, and fixed output for fifth test case

--- src/rules/escalation/rule0.js
+++ src/rules/escalation/rule0.js
@@ -1,138 +1,158 @@
 import _ from 'lodash';
 import * as get from '../library/getICSDose';
 
-const rule0 = ( patientMedications, masterMedications ) => _.chain( patientMedications )
-    .reduce( ( result, originalMedication ) => {
-      const rule = _.partial( ( _masterMedications, patientMedication ) => {
-        if ( patientMedication.chemicalType !== 'ICS' && patientMedication.chemicalType !== 'laba,ICS' ) {
-          if (
-            ( patientMedication.chemicalType === 'laba' )
-            && ( _.some( _masterMedications, { chemicalType: 'laba,ICS' } ) )
-          ) {
-            const isLabaICSAndChemicalLABA = _.chain( _masterMedications )
-              .filter( {
-                chemicalType: 'laba,ICS',
-                chemicalLABA: patientMedication.chemicalLABA,
-              } )
-              .isEmpty()
-              .value();
-            if ( patientMedication.chemicalType === 'laba' && !isLabaICSAndChemicalLABA ) {
-              return result.push( _.chain( _masterMedications )
+const rule0 = ( patientMedications, masterMedications ) => {
+  const recommendationOne = _.chain( masterMedications )
+    .filter( { name: 'flovent', doseICS: '125' } )
+    .thru( changeOne =>
+      _.map( changeOne, changeOneEach => Object.assign( changeOneEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
+    .value();
+  const recommendationTwo = _.chain( masterMedications )
+    .filter( { device: 'diskus', doseICS: '100' } )
+    .thru( changeTwo =>
+      _.map( changeTwo, changeTwoEach => Object.assign( changeTwoEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
+    .value();
+  const recommendationThree = _.chain( masterMedications )
+    .filter( { name: 'pulmicort', doseICS: '200' } )
+    .thru( changeThree =>
+      _.map( changeThree, changeThreeEach =>
+        Object.assign( changeThreeEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
+    .value();
+  const recommendationFour = _.chain( masterMedications )
+    .filter( { name: 'asmanex', doseICS: '200' } )
+    .thru( changeFour =>
+      _.map( changeFour, changeFourEach =>
+        Object.assign( changeFourEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
+    .value();
+  const recommendationFive = _.chain( masterMedications )
+    .filter( { name: 'alvesco', doseICS: '200' } )
+    .thru( changeFive =>
+      _.map( changeFive, changeFiveEach =>
+        Object.assign( changeFiveEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
+    .value();
+  const recommendationSix = _.chain( masterMedications )
+    .thru( changeSix =>
+      _.map( changeSix, changeSixEach => Object.assign( changeSixEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
+    .filter( { name: 'qvar', doseICS: '100' } )
+    .value();
+  if ( _.isEmpty( patientMedications ) ) {
+    const recommendations = [[recommendationOne,
+      recommendationTwo,
+      recommendationThree,
+      recommendationFour,
+      recommendationFive,
+      recommendationSix,
+    ]];
+
+    return _.flattenDeep( recommendations );
+  }
+
+    return _.chain( patientMedications )
+      .reduce( ( result, originalMedication ) => {
+        const rule = _.partial( ( _masterMedications, _patientMedications, patientMedication ) => {
+          const noIcsOrLabaIcs = _.chain( _patientMedications )
+            .filter( _medication => _medication === 'ICS' || _medication === 'laba,ICS' )
+            .isEmpty()
+            .value();
+          if ( noIcsOrLabaIcs ) {
+            if (
+              ( patientMedication.chemicalType === 'laba' )
+              && ( _.some( _masterMedications, { chemicalType: 'laba,ICS' } ) )
+            ) {
+              const isLabaICSAndChemicalLABA = _.chain( _masterMedications )
                 .filter( {
                   chemicalType: 'laba,ICS',
                   chemicalLABA: patientMedication.chemicalLABA,
-                  device: patientMedication.device,
                 } )
-                .thru( ( results ) => {
-                  if ( !_.isEmpty( results ) ) {
+                .isEmpty()
+                .value();
+              if ( patientMedication.chemicalType === 'laba' && !isLabaICSAndChemicalLABA ) {
+                return result.push( _.chain( _masterMedications )
+                  .filter( {
+                    chemicalType: 'laba,ICS',
+                    chemicalLABA: patientMedication.chemicalLABA,
+                    device: patientMedication.device,
+                  } )
+                  .thru( ( results ) => {
+                    if ( !_.isEmpty( results ) ) {
+                      return _.chain( _masterMedications )
+                        .filter( {
+                          chemicalType: 'laba,ICS',
+                          chemicalLABA: patientMedication.chemicalLABA,
+                          device: patientMedication.device,
+                        } )
+                        .value();
+                    }
+
                     return _.chain( _masterMedications )
                       .filter( {
                         chemicalType: 'laba,ICS',
                         chemicalLABA: patientMedication.chemicalLABA,
-                        device: patientMedication.device,
                       } )
                       .value();
-                  }
-
-                  return _.chain( _masterMedications )
-                      .filter( {
-                        chemicalType: 'laba,ICS',
-                        chemicalLABA: patientMedication.chemicalLABA,
-                      } )
-                      .value();
-                } )
-                .thru( get.lowestICSDose )
-                .thru(
-                  medication => Object.assign( {}, medication,
+                  } )
+                  .thru( get.lowestICSDose )
+                  .thru(
+                    medication => Object.assign( {}, medication,
                       { maxPuffPerTime: 1, tag: 'e2' },
-                      ) )
-                .value(),
-              );
-            }
+                    ) )
+                  .value(),
+                );
+              }
 
-            const newMedications = _.chain( _masterMedications )
-                .reduce( ( recommend, medication ) => {
+              return _.chain( _masterMedications )
+                .reduce( ( accNewMedications, medication ) => {
                   if ( medication.chemicalLABA === 'salmeterol' &&
                     medication.chemicalICS === 'fluticasone' &&
                     medication.device === 'diskus' ) {
-                    recommend.push( medication );
+                    accNewMedications.diskus.push( medication );
                   }
-                  if ( medication.chemicalLABA === 'salmeterol' &&
+                  else if ( medication.chemicalLABA === 'salmeterol' &&
                     medication.chemicalICS === 'fluticasone' &&
                     medication.device === 'inhaler2' ) {
-                    recommend.push( medication );
+                    accNewMedications.inhaler2Advair.push( medication );
                   }
-                  if ( medication.chemicalLABA === 'formoterol' && medication.chemicalICS === 'budesonide' ) {
-                    recommend.push( medication );
+                  else if ( medication.chemicalLABA === 'formoterol' &&
+                    medication.chemicalICS === 'budesonide' ) {
+                    accNewMedications.inhaler2Zenhale.push( medication );
                   }
-                  if ( medication.chemicalLABA === 'formoterol' && medication.chemicalICS === 'mometasone' ) {
-                    recommend.push( medication );
+                  else if ( medication.chemicalLABA === 'formoterol' &&
+                    medication.chemicalICS === 'mometasone' ) {
+                    accNewMedications.symbicort.push( medication );
                   }
 
-                  return recommend;
-                }, [] )
+                  return accNewMedications;
+                }, { diskus: [], inhaler2Advair: [], inhaler2Zenhale: [], symbicort: [] } )
+                .map( ( _medications ) => {
+                  return get.lowestICSDose( _medications );
+                } )
+                .map( _medication => Object.assign( _medication, { maxPuffPerTime: 1, tag: 'e3' } ) )
+                .thru( addToRecommendation => result.push( addToRecommendation ) )
                 .value();
-
-            const lowestICSDose = get.lowestICSDose( newMedications );
-            result.push( Object.assign( {}, lowestICSDose, { maxPuffPerTime: 1, tag: 'e3' } ) );
-          }
-          else {
-            const recommendationOne = _.chain( _masterMedications )
-              .filter( { name: 'flovent', doseICS: '125' } )
-              .thru( changeOne =>
-                _.map( changeOne, changeOneEach => Object.assign( changeOneEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
-              .value();
-            const recommendationTwo = _.chain( _masterMedications )
-              .filter( { device: 'diskus', doseICS: '100' } )
-              .thru( changeTwo =>
-                _.map( changeTwo, changeTwoEach => Object.assign( changeTwoEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
-              .value();
-            const recommendationThree = _.chain( _masterMedications )
-              .filter( { name: 'pulmicort', doseICS: '200' } )
-              .thru( changeThree =>
-                _.map( changeThree, changeThreeEach =>
-                  Object.assign( changeThreeEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
-              .value();
-            const recommendationFour = _.chain( _masterMedications )
-              .filter( { name: 'asmanex', doseICS: '200' } )
-              .thru( changeFour =>
-                _.map( changeFour, changeFourEach =>
-                  Object.assign( changeFourEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
-              .value();
-            const recommendationFive = _.chain( _masterMedications )
-              .filter( { name: 'alvesco', doseICS: '200' } )
-              .thru( changeFive =>
-                _.map( changeFive, changeFiveEach =>
-                  Object.assign( changeFiveEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
-              .value();
-            const recommendationSix = _.chain( _masterMedications )
-              .thru( changeSix =>
-                _.map( changeSix, changeSixEach => Object.assign( changeSixEach, { maxPuffPerTime: 1, tag: 'e1' } ) ) )
-              .filter( { name: 'qvar', doseICS: '100' } )
-              .value();
-
-            result.push( 'Options: ',
-              recommendationOne,
-              recommendationTwo,
-              recommendationThree,
-              recommendationFour,
-              recommendationFive,
-              recommendationSix,
+            }
+            else {
+              result.push( 'Options: ',
+                recommendationOne,
+                recommendationTwo,
+                recommendationThree,
+                recommendationFour,
+                recommendationFive,
+                recommendationSix,
               );
-          }
-        }
+            }
 
-        if ( patientMedication.chemicalType === 'ltra' ) {
-          result.push( patientMedication );
-        }
+            if ( patientMedication.chemicalType === 'ltra' ) {
+              result.push( patientMedication );
+            }
+          }
+
+          return result;
+        }, masterMedications, patientMedications );
+        rule( originalMedication );
 
         return result;
-      }, masterMedications );
-      rule( originalMedication );
-
-      return result;
-    }, [] )
-    .flatten()
-    .value();
-
+      }, [] )
+      .flatten()
+      .value();
+  }
 export default rule0;
