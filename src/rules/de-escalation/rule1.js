609888a1c0b3cf4719e4f4890fa6d051d3ac2075
Shirley Xiao <shirley.xiao@qochealth.com>
2017-09-07 14:03:13 -0400

    deleted commented out code for de rule1

--- src/rules/de-escalation/rule1.js
+++ src/rules/de-escalation/rule1.js
@@ -6,66 +6,10 @@ import * as calculate from '../library/calculateICSDose';
 const rule1 = ( patientMedications, masterMedications, questionnaireAnswers ) => _.chain( patientMedications )
     .reduce( ( result, medication ) => {
       const rule = _.partial( ( _masterMedications, _patientMedications, _questionnaireAnswers, patientMedication ) => {
-        // const filterMedications = _.chain( medicationElement )
-        //   .filter( findMedication => (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'flovent' &&
-        //         findMedication.device === 'inhaler2'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 200 ) ) &&
-        //         findMedication.name === 'flovent' &&
-        //         findMedication.device === 'diskus'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 200 ) ) &&
-        //         findMedication.name === 'pulmicort' &&
-        //         findMedication.device === 'turbuhaler'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'qvar' &&
-        //         findMedication.device === 'inhaler1'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'asthmanex' &&
-        //         findMedication.device === 'twisthaler'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'alvesco' &&
-        //         findMedication.device === 'inhaler1'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'arnuity' &&
-        //         findMedication.device === 'ellipta'
-        //       ) )
-        //   .value();
-
-        // const medicationsWithLowestDose = _.chain( filterMedications )
-        //   .filter(
-        //   {
-        //     chemicalType: patientMedication.chemicalType,
-        //     name: patientMedication.name,
-        //     device: patientMedication.device,
-        //   } )
-        //   .value();
         const noLabaLtra = _.chain( _patientMedications )
           .filter( _noMedication => _noMedication.chemicalType === 'laba' || _noMedication.chemicalType === 'ltra' )
           .isEmpty()
           .value();
-        // const compareLowestDoseToPatientMedication = _.chain( medicationsWithLowestDose )
-        //   .filter( _medication => calculate.patientICSDose( patientMedication ) <= calculate.ICSDose( _medication ) )
-        //   .isEmpty()
-        //   .value();
 
         const compareLowestDoseToPatientMedication =
           _.chain( _masterMedications )
