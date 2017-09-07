b6b1380c1f814b5c7f23c3b73eb08bc653675389
Shirley Xiao <shirley.xiao@qochealth.com>
2017-09-07 10:04:05 -0400

    renamed variables for de rule1

--- src/rules/de-escalation/rule1.js
+++ src/rules/de-escalation/rule1.js
@@ -5,109 +5,15 @@ import * as calculate from '../library/calculateICSDose';
 // add tag to medications to determine message for UI
 const rule1 = ( patientMedications, masterMedications, questionnaireAnswers ) => _.chain( patientMedications )
     .reduce( ( result, medication ) => {
-      const rule = _.partial( ( medicationElement, originalMedications, asthmaControlAnswers, patientMedication ) => {
-        // const filterMedications = _.chain( medicationElement )
-        //   .filter( findMedication => (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'flovent' &&
-        //         findMedication.device === 'inhaler2'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 200 ) ) &&
-        //         findMedication.name === 'flovent' &&
-        //         findMedication.device === 'diskus'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 200 ) ) &&
-        //         findMedication.name === 'pulmicort' &&
-        //         findMedication.device === 'turbuhaler'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'qvar' &&
-        //         findMedication.device === 'inhaler1'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'asthmanex' &&
-        //         findMedication.device === 'twisthaler'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'alvesco' &&
-        //         findMedication.device === 'inhaler1'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'arnuity' &&
-        //         findMedication.device === 'ellipta'
-        //       ) )
-        //   .value();
+      const rule = _.partial( ( _masterMedications, _patientMedications, _questionnaireAnswers, patientMedication ) => {
 
-        // const medicationsWithLowestDose =
-        //   _.chain( medicationElement )
-        //     .filter( findMedication => (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'flovent' &&
-        //         findMedication.device === 'inhaler2'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 200 ) ) &&
-        //         findMedication.name === 'flovent' &&
-        //         findMedication.device === 'diskus'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 200 ) ) &&
-        //         findMedication.name === 'pulmicort' &&
-        //         findMedication.device === 'turbuhaler'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'qvar' &&
-        //         findMedication.device === 'inhaler1'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'asthmanex' &&
-        //         findMedication.device === 'twisthaler'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'alvesco' &&
-        //         findMedication.device === 'inhaler1'
-        //       )
-        //       ||
-        //       (
-        //         !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
-        //         findMedication.name === 'arnuity' &&
-        //         findMedication.device === 'ellipta'
-        //       ) )
-        //   .filter(
-        //   {
-        //     chemicalType: patientMedication.chemicalType,
-        //     name: patientMedication.name,
-        //     device: patientMedication.device,
-        //   } )
-        //   .value();
-
-        const noLabaLtra = _.chain( originalMedications )
+        const noLabaLtra = _.chain( _patientMedications )
           .filter( _noMedication => _noMedication.chemicalType === 'laba' || _noMedication.chemicalType === 'ltra' )
           .isEmpty()
           .value();
 
         const compareLowestDoseToPatientMedication =
-          _.chain( medicationElement )
+          _.chain( _masterMedications )
             .filter( findMedication => (
                 !_.isNil( adjust.ICSDoseToOriginalMedication( findMedication, 100 ) ) &&
                 findMedication.name === 'flovent' &&
@@ -154,13 +60,12 @@ const rule1 = ( patientMedications, masterMedications, questionnaireAnswers ) =>
               name: patientMedication.name,
               device: patientMedication.device,
               } )
-            .filter( _medication => {console.log('medications: ', _medication );  return calculate.patientICSDose( patientMedication ) <= calculate.ICSDose( _medication ) } )
+            .filter( _medication => calculate.patientICSDose( patientMedication ) <= calculate.ICSDose( _medication ) )
             .isEmpty()
             .value();
-        console.log( 'compareLowestDoseToPatientMedication: ', compareLowestDoseToPatientMedication);
 
         if ( patientMedication.chemicalType === 'ICS' && noLabaLtra && !compareLowestDoseToPatientMedication ) {
-          const avgAsthmaSymptoms = asthmaControlAnswers[0].asthmaSymptoms;
+          const avgAsthmaSymptoms = _questionnaireAnswers[0].asthmaSymptoms;
           if ( avgAsthmaSymptoms === '0' ) {
             return result.push( 'statement1Ai',
               Object.assign( patientMedication, { maxPuffPerTime: patientMedication.puffPerTime, tag: 'd1' } ) );
